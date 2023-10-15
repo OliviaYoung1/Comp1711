c06717471ed12b4bde19157efee7b097a9e2fa1d
Olivia Young <146246572+OliviaYoung1@users.noreply.github.com>
2023-10-15 20:49:21 +0000

    worksheet 2

--- week2_c_bootcamp/worksheet2.c
+++ week2_c_bootcamp/worksheet2.c
@@ -1,4 +1,53 @@
 #include <stdio.h>
 int main(){
+//task 1
+    //int a[] = {1,2,3,4,5};
+    //int total=0, count;
+    //for (count= 0; count < 5; count++)
+    //{
+    //    total = total + a[count];
+    //}
+    //printf("%d\n", total);
     
+ //task 2
+    //int a[] = {1,2,3,4,5};
+    //int i;
+    //for (i=4; i >= 0; i--)
+    //{
+    //    printf("%d\n", a[i]);
+    //}    
+
+ //task 3
+    //int a[] = {1,2,7,4,5};
+    //int i, max = a[1];   
+    //for (i=0; i < 5; i++)
+    //{
+    //   if (a[i] > max)
+    //   {
+    //    max = a[i];
+    //   }
+    //}
+    //printf("%d\n", max);
+
+//task 4
+    //int a[] = {1,2,3,4,5};
+    //int i, temp;
+    //for(i=0; i<5; i++)
+    //{
+      //  if (i=4)
+        //{
+          //  a[i] = temp;
+        //}
+        //else{
+        //temp = a[i];
+        //a[i] = a[i+1];
+        //}
+    //}
+    //for(i=0; i<5; i++)
+    //{
+    //printf("%d\n", a[i]);
+    //}
+
+//task 5
+    return 0;
 }
