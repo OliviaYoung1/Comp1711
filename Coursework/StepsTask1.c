ba5f812b94486400da201b4cfcdba6a9b9ceaadc
Olivia Young <146246572+OliviaYoung1@users.noreply.github.com>
2023-10-29 14:30:57 +0000

    courswork failure

--- Coursework/StepsTask1.c
+++ Coursework/StepsTask1.c
@@ -10,8 +10,7 @@ typedef struct {
 } FITNESS_DATA;
 
 // Define any additional variables here
-char record= [];
-int i;
+
 
 
 // This is your helper function. Do not change it in any way.
@@ -44,35 +43,22 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
-    //char record = [[],[],[]];
-    int rows = 0; //holds the number of records
 
     char filename [] = "FitnessData_2023.csv";
-    FILE *file = fopen(filename, "r"); 
+    FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("");
         return 1;
     }
 
-    int buffer_size = 100; 
+    int buffer_size = 10000;
     char line_buffer[buffer_size];
-    while (fgets(line_buffer, buffer_size, file) != NULL){
-        printf("%s", line_buffer);
-        record.append(line_buffer);
-        rows++;
-    int records_to_print = 0;
-    for (i==0; i == rows; i++){
-        tokeniseRecord(record[i], ",", date, time, steps);
-        records_to_print++;
-        if (records_to_print < 4){
-            printf("%s/%s/%s", date, time, steps);
-        }
+    while (fgets(line_buffer, buffer_size, file) != NULL) {
+        printf("%d\n", atoi(line_buffer));
     }
-    }
+
     fclose(file);
-    printf("Number of records in file: %d\n", rows);
     return 0;
-    
-
-
 }
+
+
