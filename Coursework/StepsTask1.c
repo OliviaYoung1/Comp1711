37ecfc55e382681549f9598ed603086842905af9
Olivia Young <146246572+OliviaYoung1@users.noreply.github.com>
2023-11-02 21:53:45 +0000

    finished coursework

--- Coursework/StepsTask1.c
+++ Coursework/StepsTask1.c
@@ -10,7 +10,7 @@ typedef struct {
 } FITNESS_DATA;
 
 // Define any additional variables here
-
+int rows = 0; //holds the number of records
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
@@ -41,14 +41,14 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
-    int rows = 0; //holds the number of records
-
+    //reading the file
     char filename [] = "FitnessData_2023.csv";
     FILE *file = fopen(filename, "r"); 
     if (file == NULL) {
         perror("");
         return 1;
     }
+    //adding all rows into data array and counting rows
     char data[100][100];
     int buffer_size = 100; 
     char line_buffer[buffer_size];
@@ -58,24 +58,26 @@ int main() {
     fclose(file);
     printf("Number of records in file: %d\n", rows);
 
-    FITNESS_DATA FITNESS_DATAS[rows];
-    int x;
-    for (x = 0; x < rows ; x++){
-       char date[11];
-       char time[6];
-       char steps[10]; 
-       //printf("%s", data[x]); 
-       tokeniseRecord(data[x], ",", date, time, steps);
-        FITNESS_DATAS[x].date == date;
-        FITNESS_DATAS[x].time == time;
-        FITNESS_DATAS[x].steps == 10;
-        printf("%s\n", FITNESS_DATAS[x].date);
-
+    //tokensing the records and adding to the structue
+    FITNESS_DATA FITNESS_DATAS [rows];
+    int a;
+    for (a = 0; a < rows; a++)
+    {
+        char date[11];
+        char time[6];
+        char steps[10];
+        tokeniseRecord(data[a], ",", date, time, steps);
+        strcpy(FITNESS_DATAS[a].date, date);
+        strcpy(FITNESS_DATAS[a].time, time);
+        FITNESS_DATAS[a].steps = atoi(steps);
     }
-    //printf("%s/%s/%d", FITNESS_DATAS[0].date, FITNESS_DATAS[0].time, FITNESS_DATAS[0].steps);
-    //printf("%s/%s/%d", FITNESS_DATAS[1].date, FITNESS_DATAS[1].time, FITNESS_DATAS[1].steps);
-    //printf("%s/%s/%d", FITNESS_DATAS[2].date, FITNESS_DATAS[2].time, FITNESS_DATAS[2].steps);
+
+    printf("%s/%s/%d\n", FITNESS_DATAS[0].date, FITNESS_DATAS[0].time, FITNESS_DATAS[0].steps);
+    printf("%s/%s/%d\n", FITNESS_DATAS[1].date, FITNESS_DATAS[1].time, FITNESS_DATAS[1].steps);
+    printf("%s/%s/%d\n", FITNESS_DATAS[2].date, FITNESS_DATAS[2].time, FITNESS_DATAS[2].steps);
+    
     return 0;
+
+
+    
 }
-
-
